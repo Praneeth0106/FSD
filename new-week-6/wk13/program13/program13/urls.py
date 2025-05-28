34090f7d0372f68f73956009b4c1a1a74311505f
praneeth0106 <praneethkumar0106@gmail.com>
2025-05-28 17:14:31 +0530

    Delete new-week-6/wk13/program13 directory

--- new-week-6/wk13/program13/program13/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import index
-
-urlpatterns = [
-    path('', index.home, name='home'),
-    path('dashboard/', index.dashboard, name='dashboard'),
-    path('contactUs/', index.contactUs, name='contactUs'),
-]
