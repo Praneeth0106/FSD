e0f69c5e3fe72bbc7f529ba43c7484a87848d228
praneeth0106 <praneethkumar0106@gmail.com>
2025-05-28 17:14:04 +0530

    Delete new-week-6/week 11/my-app directory

--- new-week-6/week 11/my-app/src/ContactUs.js
+++
@@ -1,5 +0,0 @@
-const ContactUs = () => {
-  return <h1>CMRIT, Kandlakoya Village, Medchal, Hyderabad</h1>;
-};
-
-export default ContactUs;
--- new-week-6/week 11/my-app/src/Home.js
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <h1>Welcome to CMRIT Home Page</h1>;
-};
-
-export default Home;
